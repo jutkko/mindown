b0aec8171675436a9a496a46eb6f10fec61b0766
Juto Yu <jutkko@gmail.com>
2016-09-11 11:10:36 +0100

    Vendor ginkgo and gomega

--- input/ompl_test.go
+++ input/ompl_test.go
@@ -1,12 +1,12 @@
 package input_test
 
 import (
-	. "github.com/jutkko/mindown/input"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Ompl", func() {
-
+	It("should compile", func() {
+		Expect(true).To(Equal(true))
+	})
 })
