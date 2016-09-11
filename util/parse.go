2f882b94b91853527c744577ad52b62015256384
Juto Yu <jutkko@gmail.com>
2016-09-11 22:31:27 +0100

    Implement graph

--- util/parse.go
+++ util/parse.go
@@ -1,14 +1,31 @@
 package util
 
-import "os"
+type Node struct {
+	children []*Node
+	title    string
+	value    string
+}
+
+func NewNode(title, value string) *Node {
+	return &Node{
+		title: title,
+		value: value,
+	}
+}
+
+func (n *Node) AddChild(childNode *Node) {
+	n.children = append(n.children, childNode)
+}
+
+func (n *Node) GetChildren() []*Node {
+	return n.children
+}
 
 type Graph struct {
+	nodes []*Node
 }
 
+// TODO: implement this
 func (g *Graph) Export() error {
 	return nil
-}
-
-type Parser interface {
-	Parse(file *os.File) *Graph
 }
---
+++ util/parse_test.go
@@ -0,0 +1,37 @@
+package util_test
+
+import (
+	. "github.com/jutkko/mindown/util"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Parse", func() {
+	var (
+		node1, node2, node3 *Node
+	)
+
+	BeforeEach(func() {
+		node1 = NewNode("title1", "value1")
+		node2 = NewNode("title2", "value2")
+		node3 = NewNode("title3", "value3")
+	})
+
+	It("adds the correct child to existing node", func() {
+		node1.AddChild(node2)
+		node1Children := node1.GetChildren()
+		Expect(node1Children).Should(ConsistOf(node2))
+	})
+
+	Context("adding multiple children", func() {
+
+		It("adds the correct children to the existing node", func() {
+			node1.AddChild(node2)
+			node1.AddChild(node3)
+			node1Children := node1.GetChildren()
+			Expect(node1Children).Should(ConsistOf(node2, node3))
+		})
+	})
+
+})
