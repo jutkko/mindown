c24096eee81c26ce6c872abab6f4af22a59d661f
Juto Yu <jutkko@gmail.com>
2016-09-14 13:27:25 +0100

    Add check for nil

--- output/markdown.go
+++ output/markdown.go
@@ -9,7 +9,10 @@ import (
 )
 
 func WriteMarkdown(filename string, graph *util.Graph) error {
-	err := ioutil.WriteFile(filename, []byte(fmt.Sprintf("%s\n", graph.GetNodes()[0].GetTitle())), 0644)
+	if graph == nil {
+		return errors.New("Graph is nil")
+	}
+	fmt.Printf("Output %s\n\n", fmt.Sprintf("%s\n", graph.GetNodes()[0].GetTitle()))
 	err := ioutil.WriteFile(filename, []byte(fmt.Sprintf("# %s\n", graph.GetNodes()[0].GetTitle())), 0644)
 
 	if err != nil {
