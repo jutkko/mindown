805c3a7ba666049bffc3adcddbbc03ab4a9952ea
Juto Yu <jutkko@gmail.com>
2016-09-14 13:26:57 +0100

    Add markdown specific "#"

--- output/markdown.go
+++ output/markdown.go
@@ -10,6 +10,7 @@ import (
 
 func WriteMarkdown(filename string, graph *util.Graph) error {
 	err := ioutil.WriteFile(filename, []byte(fmt.Sprintf("%s\n", graph.GetNodes()[0].GetTitle())), 0644)
+	err := ioutil.WriteFile(filename, []byte(fmt.Sprintf("# %s\n", graph.GetNodes()[0].GetTitle())), 0644)
 
 	if err != nil {
 		return errors.New(fmt.Sprintf("Failed to write file: %s %s", filename, err.Error()))
