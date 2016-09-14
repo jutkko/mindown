df839687c314f05cb59d7cc97d8f15122511762b
Juto Yu <jutkko@gmail.com>
2016-09-14 01:26:27 +0100

    More work on markdown, it's getting there

--- output/markdown.go
+++ output/markdown.go
@@ -1,15 +1,19 @@
 package output
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 
 	"github.com/jutkko/mindown/util"
 )
 
 func WriteMarkdown(filename string, graph *util.Graph) error {
-	err := ioutil.WriteFile(filename, []byte("# Vim-notes"), 0644)
+	err := ioutil.WriteFile(filename, []byte(fmt.Sprintf("%s\n", graph.GetNodes()[0].GetTitle())), 0644)
+
 	if err != nil {
-		panic("Cannot write " + err.Error())
+		return errors.New(fmt.Sprintf("Failed to write file: %s %s", filename, err.Error()))
 	}
+
 	return nil
 }
--- output/markdown_test.go
+++ output/markdown_test.go
@@ -23,7 +23,28 @@ var _ = Describe("Markdown", func() {
 		output.WriteMarkdown(tempFile.Name(), graph)
 		content := make([]byte, 20)
 		tempFile.Read(content)
-		Expect(content).Should(ContainSubstring("# Vim-notes"))
+		Expect(content).Should(ContainSubstring("# Vim-notes\n"))
 	})
 
+	It("should output the correct file given simple-two-root graph", func() {
+		graph, err := input.ParseOpml("../testdata/simple-two-root.opml")
+		Expect(err).NotTo(HaveOccurred())
+
+		tempFile, err := ioutil.TempFile("", "")
+		Expect(err).NotTo(HaveOccurred())
+
+		fmt.Printf("%s\n", tempFile.Name())
+		output.WriteMarkdown(tempFile.Name(), graph)
+		content := make([]byte, 20)
+		tempFile.Read(content)
+		Expect(content).Should(ContainSubstring("# Vim-notes\n"))
+		Expect(content).Should(ContainSubstring("# Vim-notes1\n"))
+	})
+
+	Context("when the file cannot be written to", func() {
+		It("should return an error", func() {
+			err := output.WriteMarkdown("-/some-random-file-name", nil)
+			Expect(err).To(HaveOccurred())
+		})
+	})
 })
