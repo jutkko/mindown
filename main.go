197738e580f8a58027a20e2342c7f017db553ca0
Juto Yu <jutkko@gmail.com>
2016-10-09 21:21:40 +0100

    Add support for multiple format flags

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/jutkko/mindown/input"
 	"github.com/jutkko/mindown/output"
+	"github.com/jutkko/mindown/util"
 	"github.com/urfave/cli"
 )
 
@@ -14,6 +16,11 @@ func main() {
 	app.Name = "mindown"
 	app.Usage = "convert mind to files"
 	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "input-format",
+			Value: "opml",
+			Usage: "the input file format",
+		},
 		cli.StringFlag{
 			Name:  "input-file",
 			Value: "input.txt",
@@ -31,8 +38,24 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
-		if err != nil {
-			return err
+		var (
+			graph *util.Graph
+			err   error
+		)
+
+		switch c.String("input-format") {
+		case "yml":
+			graph, err = input.ParseYaml(c.String("input-file"))
+			if err != nil {
+				return err
+			}
+		case "opml":
+			graph, err = input.ParseOpml(c.String("input-file"))
+			if err != nil {
+				return err
+			}
+		default:
+			return errors.New(fmt.Sprintf("input-format not supported: %s", c.String("input-format")))
 		}
 
 		err = output.WriteMarkdown(c.String("output-file"), c.Bool("f"), graph)
